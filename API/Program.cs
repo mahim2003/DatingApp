4e6a7d1d4ab67e844e28726750c1659dff222d3e
mca265 <mahimchaudhry4@gmail.com>
2024-03-19 17:08:59 -0700

    Create a base api controller

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-app.MapControllers();
+app.MapControllers(); 
 
 app.Run();
