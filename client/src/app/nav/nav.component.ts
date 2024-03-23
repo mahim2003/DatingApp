7ae7b70af8a308c39f8da2af2e48c0f64a3f80da
mca265 <mahimchaudhry4@gmail.com>
2024-03-22 23:26:27 -0700

    Implement login persistence

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -8,25 +10,26 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn = false;
+  
 
-  constructor(private accountService: AccountService){ }
+  constructor(public accountService: AccountService){ }
 
   ngOnInit(): void {
+   
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
+        
       },
       error: error => console.log(error)
     })
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 
 }
