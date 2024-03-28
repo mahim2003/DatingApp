a97cfa7788eafcd9dfb205b7f2b51c1e2e04c95c
mca265 <mahimchaudhry4@gmail.com>
2024-03-27 19:14:45 -0700

    Create error controller for testing errors and set password validation

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace API.DTOs
         public string Username { get; set;}
 
         [Required]
+        [StringLength(18, MinimumLength = 8)]
         public string Password { get; set;}
     }
 }
