ae46a745cca802ed721095f82b4f6aca23dcf672
mca265 <mahimchaudhry4@gmail.com>
2024-04-04 15:00:00 -0700

    Seed data into database

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-[AllowAnonymous]
+[Authorize]
 public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
