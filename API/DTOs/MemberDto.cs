968a109084c65ae962bbef983778e161c862a921
mca265 <mahimchaudhry4@gmail.com>
2024-04-05 20:41:56 -0700

    Add and configure AutoMapper

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -2,9 +2,11 @@ namespace API.DTOs
 {
     public class MemberDto
     {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
     public string UserName { get; set; }
+
+    public string PhotoUrl { get; set; }
 
     public int age { get; set; }
 
